e5276551f6cd58cf3fe31efe7676073ef951961e
Shawn Fan <aasfg1234@gmail.com>
2021-04-07 23:02:19 +0800

    add average

--- read.py
+++ read.py
@@ -4,11 +4,14 @@ with open('reviews.txt', 'r') as f:  #with可以自動關閉讀取檔案
 	for line in f:
 		data.append(line)
 		count += 1
-		if  count % 1000 == 0:  # 求餘數時使用%
+		if  count % 100000 == 0:  # 求餘數時使用%
 			print(len(data))
 
-print(len(data))
+print('檔案讀取完ㄌ，總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------------------------------------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	#print(sum_len)
+
+print('留言的平均長度為', sum_len/len(data))
