7a830fcd75ad13b28000932d28d1d8fcba7d80c2
Shawn Fan <aasfg1234@gmail.com>
2021-04-10 18:55:00 +0800

    add random filter function

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
+import random
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:  #with可以自動關閉讀取檔案
@@ -15,3 +16,16 @@ for d in data:
 	#print(sum_len)
 
 print('留言的平均長度為', sum_len/len(data))
+
+s = 1
+y = 21741
+z = random.randint(s, y)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(z)
+print('一共有', len(new), '筆留言長度小於100')
+print('第', z, '筆資料為: ')
+print(new[z])
